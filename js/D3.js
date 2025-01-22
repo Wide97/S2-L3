50aee92eb44193952172ccf5a98c3f490a2ccd8c
Wide97 <mwidesott@gmail.com>
2025-01-22 16:13:00 +0100

    es 3

--- js/D3.js
+++ js/D3.js
@@ -125,6 +125,20 @@ console.log(charactersNames)
   Seguendo i passaggi precedenti crea un nuovo array chiamato "femaleCharacters" e inserisci al suo interno tutti gli oggetti femminili.
 */
 
+let femaleCharacters = [];
+
+for (i=0; i<starWarsCharacters.length; i ++) {
+
+  if (starWarsCharacters[i].gender === "female") {
+    
+    femaleCharacters.push(starWarsCharacters[i]);
+
+  }
+
+}
+
+console.log(femaleCharacters)
+
 /* ESERCIZIO 4
   Crea un oggetto "eyeColor" che abbia le seguenti proprietà: blue, yellow, brown, red, blue-gray.
   Ad ognuna di queste proprietà assegna come valore un array vuoto.
